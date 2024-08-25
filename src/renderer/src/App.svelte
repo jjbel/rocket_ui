95ea20bb5d4874f5048907669ef82586933a7e40
jjbel <jaibellare@gmail.com>
2024-08-25 18:33:14 +0530

    add Plot

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  import Plot from './components/Plot.svelte'
 </script>
 
 <h1>Rocket UI</h1>
+
+<div id="tester" style="width:600px;height:250px;"></div>
+
+<Plot />
---
+++ src/renderer/src/components/Plot.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import Plotly from 'plotly.js-dist'
+
+  onMount(() => {
+    const TESTER = document.getElementById('tester')
+
+    Plotly.newPlot(
+      TESTER,
+      [
+        {
+          x: [1, 2, 3, 4, 5],
+          y: [1, 2, 4, 8, 16]
+        }
+      ],
+      {
+        margin: { t: 0 }
+      }
+    )
+  })
+</script>
+
+<div>Caption</div>
